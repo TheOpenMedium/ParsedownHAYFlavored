81205b05f82feabff0e0a61c8a4f80acfd793e82
Lyes Saadi <mail@lyes.eu>
2018-09-13 15:23:36 +0100

    Adding support for Checkbox and Radio.

--- ParsedownHAYFlavored.php
+++ ParsedownHAYFlavored.php
@@ -1,8 +1,7 @@
 <?php
-
 class ParsedownHAYFlavored extends ParsedownExtra
 {
-    const version = '0.0.1';
+    const version = '0.1.0';
 
     function __construct()
     {
@@ -13,7 +12,7 @@ class ParsedownHAYFlavored extends ParsedownExtra
 
     protected function blockList($Line)
     {
-        if (preg_match('/^- ?\[([xX ]?)\] ?(.*)$/m', $Line['text']) || preg_match('/^- ?\(([xX ]?)\) ?(.*)$/m', $Line['text'])) {
+        if (preg_match('/^- ?\[([xX ]?)\](?::["\'](.+)["\'])? ?(.*)$/m', $Line['text']) || preg_match('/^- ?\(([xX ]?)\)(?::["\'](.+)["\'])? ?(.*)$/m', $Line['text'])) {
             return;
         } else {
             return parent::blockList($Line);
@@ -22,33 +21,57 @@ class ParsedownHAYFlavored extends ParsedownExtra
 
     protected function inlineCheckbox($excerpt)
     {
-        if (preg_match('/^- ?\[([xX ]?)\] ?(.*)$/m', $excerpt['text'], $matches))
+        if (preg_match('/^- ?\[([xX ]?)\](?::["\'](.+)["\'])? ?(.*)$/m', $excerpt['text'], $matches))
         {
-            return array(
+            $result = array(
                 'extent' => strlen($matches[0]),
                 'element' => array(
                     'name' => 'label',
-                    'text' => $matches[2],
-                    'attributes' => array(
+                    'elements' => array(
+                        array('name' => 'input', 'attributes' => array('type' => 'checkbox')),
+                        array('text' => $matches[3]),
+                        array('name' => 'br'),
                     )
                 )
             );
+
+            if ($matches[1] == "x" || $matches[1] == "X") {
+                $result["element"]["elements"][0]["attributes"]["checked"] = "";
+            }
+
+            if ($matches[2]) {
+                $result["element"]["elements"][0]["attributes"]["name"] = $matches[2];
+            }
+
+            return $result;
         }
     }
 
     protected function inlineRadio($excerpt)
     {
-        if (preg_match('/^- ?\(([xX ]?)\) ?(.*)$/m', $excerpt['text'], $matches))
+        if (preg_match('/^- ?\(([xX ]?)\)(?::["\'](.+)["\'])? ?(.*)$/m', $excerpt['text'], $matches))
         {
-            return array(
+            $result = array(
                 'extent' => strlen($matches[0]),
                 'element' => array(
                     'name' => 'label',
-                    'text' => $matches[2],
-                    'attributes' => array(
+                    'elements' => array(
+                        array('name' => 'input', 'attributes' => array('type' => 'radio')),
+                        array('text' => $matches[3]),
+                        array('name' => 'br'),
                     )
                 )
             );
+
+            if ($matches[1] == "x" || $matches[1] == "X") {
+                $result["element"]["elements"][0]["attributes"]["checked"] = "";
+            }
+
+            if ($matches[2]) {
+                $result["element"]["elements"][0]["attributes"]["name"] = $matches[2];
+            }
+
+            return $result;
         }
     }
 }
