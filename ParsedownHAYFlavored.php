cdee5ef8331e43d2e1b0370d2c187c5e331c021a
Lyes Saadi <mail@lyes.eu>
2018-09-30 21:18:55 +0100

    Adding Survey support.

--- ParsedownHAYFlavored.php
+++ ParsedownHAYFlavored.php
@@ -1,13 +1,15 @@
 <?php
 class ParsedownHAYFlavored extends ParsedownExtra
 {
-    const version = '0.1.0';
+    const version = '0.1.1';
 
     function __construct()
     {
         $this->InlineTypes['-'][]= 'Checkbox';
         $this->InlineTypes['-'][]= 'Radio';
+        $this->InlineTypes['['][]= 'Survey';
         $this->inlineMarkerList .= '-';
+        $this->inlineMarkerList .= '[';
     }
 
     protected function blockList($Line)
@@ -70,6 +72,34 @@ class ParsedownHAYFlavored extends ParsedownExtra
             if ($matches[2]) {
                 $result["element"]["elements"][0]["attributes"]["name"] = $matches[2];
             }
+
+            return $result;
+        }
+    }
+
+    protected function inlineSurvey($excerpt)
+    {
+        if (preg_match("/\[survey ?(\d+) ?\/\]/i", $excerpt['text'], $matches)) {
+            $localeList = preg_split('#[,;-]#', $_SERVER['HTTP_ACCEPT_LANGUAGE']);
+
+            foreach ($localeList as $locale) {
+                if ($locale == 'en' || $locale == 'fr') {
+                    $l = $locale;
+                    break;
+                }
+            }
+            if (empty($l)) {
+                $l = 'en';
+            }
+
+            $root = (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . ':' . $_SERVER['SERVER_PORT'] . '/';
+            $url = $root . $l . '/display/survey/' . $matches[1];
+            $result = array(
+                'extent' => strlen($matches[0]),
+                'element' => array(
+                    'rawHtml' => file_get_contents($url)
+                )
+            );
 
             return $result;
         }
